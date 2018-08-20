1982f61018f3df32909954e6353c18b92d9cf74b
lipeng <734991033@qq.com>
2018-08-20 20:30:35 +0800

    ~

--- src/cn/gov/gdupi/Application.java
+++ src/cn/gov/gdupi/Application.java
@@ -20,12 +20,12 @@ public class Application {
         return args -> {
 
             System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
+//
+//            String[] beanNames = ctx.getBeanDefinitionNames();
+//            Arrays.sort(beanNames);
+//            for (String beanName : beanNames) {
+//                System.out.println(beanName);
+//            }
 
         };
     }
