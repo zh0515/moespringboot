c2ddb2fc84910930fdc3c00124cc5bcbcf099864
lipeng <734991033@qq.com>
2018-08-30 21:27:35 +0800

    mv folder

--- quickmodel.sh
+++ quickmodel.sh
@@ -1,25 +1,25 @@
 lowercasename=$(echo ${1}| tr '[A-Z]'  '[a-z]')
 echo ${lowercasename}
-echo will make file:src/main/java/cn/gov/cn.gov.gdupi/model/${1}.java
-cp src/main/java/cn/gov/cn.gov.gdupi/model/Base.java src/main/java/cn/gov/cn.gov.gdupi/model/${1}.java
-sed -i "s/Base/${1}/g" src/main/java/cn/gov/cn.gov.gdupi/model/${1}.java
-sed -i "s/base/${lowercasename}/g" src/main/java/cn/gov/cn.gov.gdupi/model/${1}.java
+echo will make file:src/main/java/cn/gov/gdupi/model/${1}.java
+cp src/main/java/cn/gov/gdupi/model/Base.java src/main/java/cn/gov/gdupi/model/${1}.java
+sed -i "s/Base/${1}/g" src/main/java/cn/gov/gdupi/model/${1}.java
+sed -i "s/base/${lowercasename}/g" src/main/java/cn/gov/gdupi/model/${1}.java
 
-echo will make file:src/main/java/cn/gov/cn.gov.gdupi/controller/${1}Controller.java
-cp src/main/java/cn/gov/cn.gov.gdupi/controller/BaseController.java src/main/java/cn/gov/cn.gov.gdupi/controller/${1}Controller.java
-sed -i "s/Base/${1}/g" src/main/java/cn/gov/cn.gov.gdupi/controller/${1}Controller.java
-sed -i "s/base/${lowercasename}/g" src/main/java/cn/gov/cn.gov.gdupi/controller/${1}Controller.java
+echo will make file:src/main/java/cn/gov/gdupi/controller/${1}Controller.java
+cp src/main/java/cn/gov/gdupi/controller/BaseController.java src/main/java/cn/gov/gdupi/controller/${1}Controller.java
+sed -i "s/Base/${1}/g" src/main/java/cn/gov/gdupi/controller/${1}Controller.java
+sed -i "s/base/${lowercasename}/g" src/main/java/cn/gov/gdupi/controller/${1}Controller.java
 
 
-echo will make file:src/main/java/cn/gov/cn.gov.gdupi/dao/read/R${1}Mapper.java
-cp src/main/java/cn/gov/cn.gov.gdupi/dao/read/RBaseMapper.java src/main/java/cn/gov/cn.gov.gdupi/dao/read/R${1}Mapper.java
-sed -i "s/Base/${1}/g" src/main/java/cn/gov/cn.gov.gdupi/dao/read/R${1}Mapper.java
-sed -i "s/base/${lowercasename}/g" src/main/java/cn/gov/cn.gov.gdupi/dao/read/R${1}Mapper.java
+echo will make file:src/main/java/cn/gov/gdupi/dao/read/R${1}Mapper.java
+cp src/main/java/cn/gov/gdupi/dao/read/RBaseMapper.java src/main/java/cn/gov/gdupi/dao/read/R${1}Mapper.java
+sed -i "s/Base/${1}/g" src/main/java/cn/gov/gdupi/dao/read/R${1}Mapper.java
+sed -i "s/base/${lowercasename}/g" src/main/java/cn/gov/gdupi/dao/read/R${1}Mapper.java
 
-echo will make file:src/main/java/cn/gov/cn.gov.gdupi/dao/write/W${1}Mapper.java
-cp src/main/java/cn/gov/cn.gov.gdupi/dao/write/WBaseMapper.java src/main/java/cn/gov/cn.gov.gdupi/dao/write/W${1}Mapper.java
-sed -i "s/Base/${1}/g" src/main/java/cn/gov/cn.gov.gdupi/dao/write/W${1}Mapper.java
-sed -i "s/base/${lowercasename}/g" src/main/java/cn/gov/cn.gov.gdupi/dao/write/W${1}Mapper.java
+echo will make file:src/main/java/cn/gov/gdupi/dao/write/W${1}Mapper.java
+cp src/main/java/cn/gov/gdupi/dao/write/WBaseMapper.java src/main/java/cn/gov/gdupi/dao/write/W${1}Mapper.java
+sed -i "s/Base/${1}/g" src/main/java/cn/gov/gdupi/dao/write/W${1}Mapper.java
+sed -i "s/base/${lowercasename}/g" src/main/java/cn/gov/gdupi/dao/write/W${1}Mapper.java
 
 
 echo will make file:src/main/resources/cn/gov/gdupi/dao/read/R${1}Mapper.xml
@@ -33,7 +33,7 @@ sed -i "s/Base/${1}/g" src/main/resources/cn/gov/gdupi/dao/write/W${1}Mapper.xml
 sed -i "s/base/${lowercasename}/g" src/main/resources/cn/gov/gdupi/dao/write/W${1}Mapper.xml
 
 
-echo will make file:src/main/java/cn/gov/cn.gov.gdupi/service/${1}Service.java
-cp src/main/java/cn/gov/cn.gov.gdupi/service/BaseService.java src/main/java/cn/gov/cn.gov.gdupi/service/${1}Service.java
-sed -i "s/Base/${1}/g" src/main/java/cn/gov/cn.gov.gdupi/service/${1}Service.java
-sed -i "s/base/${lowercasename}/g" src/main/java/cn/gov/cn.gov.gdupi/service/${1}Service.java
+echo will make file:src/main/java/cn/gov/gdupi/service/${1}Service.java
+cp src/main/java/cn/gov/gdupi/service/BaseService.java src/main/java/cn/gov/gdupi/service/${1}Service.java
+sed -i "s/Base/${1}/g" src/main/java/cn/gov/gdupi/service/${1}Service.java
+sed -i "s/base/${lowercasename}/g" src/main/java/cn/gov/gdupi/service/${1}Service.java
